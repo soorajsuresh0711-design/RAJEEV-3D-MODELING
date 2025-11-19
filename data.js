093e81e7ca91b6a1443053d92a4910298aa5c648
soorajsuresh0711-design <soorajsuresh0711@gmail.com>
2025-11-19 12:42:42 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,287 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living--tv-unit",
+      "name": "Living & TV unit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.00934443781437011,
+        "pitch": -0.03284913539591727,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1910798110418206,
+          "pitch": -0.00037434702335659154,
+          "rotation": 0.7853981633974483,
+          "target": "1-wash-unit"
+        },
+        {
+          "yaw": 0.8620812275135812,
+          "pitch": -0.05360982573018269,
+          "rotation": 5.497787143782138,
+          "target": "5-bedroom-2"
+        },
+        {
+          "yaw": -0.643071254946598,
+          "pitch": -0.0496714448419695,
+          "rotation": 6.283185307179586,
+          "target": "3-master-bedroom"
+        },
+        {
+          "yaw": -1.3370096081236902,
+          "pitch": -0.19972802341978557,
+          "rotation": 5.497787143782138,
+          "target": "2-kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-wash-unit",
+      "name": "Wash Unit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.008170504621016761,
+        "pitch": 0.006568476356283881,
+        "fov": 0.9809471194929957
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5244224403532574,
+          "pitch": 0.039477005635228934,
+          "rotation": 0.7853981633974483,
+          "target": "4-bedroom-3"
+        },
+        {
+          "yaw": -0.4490097119494614,
+          "pitch": 0.007829931868037932,
+          "rotation": 4.71238898038469,
+          "target": "5-bedroom-2"
+        },
+        {
+          "yaw": 1.8122898398888747,
+          "pitch": 0.2985779763594163,
+          "rotation": 6.283185307179586,
+          "target": "0-living--tv-unit"
+        },
+        {
+          "yaw": -2.654900420529465,
+          "pitch": 0.03946839363704413,
+          "rotation": 1.5707963267948966,
+          "target": "3-master-bedroom"
+        },
+        {
+          "yaw": 3.0542303118393335,
+          "pitch": -0.012939957188310203,
+          "rotation": 5.497787143782138,
+          "target": "2-kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.0164020765371653,
+        "pitch": 0.00040399344033836826,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.483865221558286,
+          "pitch": 0.14015245703521195,
+          "rotation": 4.71238898038469,
+          "target": "3-master-bedroom"
+        },
+        {
+          "yaw": -1.877553029054381,
+          "pitch": 0.1697749823380974,
+          "rotation": 1.5707963267948966,
+          "target": "0-living--tv-unit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-master-bedroom",
+      "name": "Master Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.016028533946890633,
+        "pitch": 0.08904913527494784,
+        "fov": 1.2597750393917266
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0478644418409822,
+          "pitch": 0.13435717551586634,
+          "rotation": 0,
+          "target": "0-living--tv-unit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bedroom-3",
+      "name": "Bedroom 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.45236084694548495,
+        "pitch": 0.05086992919386191,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7431935678623045,
+          "pitch": 0.09424555286471126,
+          "rotation": 0,
+          "target": "0-living--tv-unit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bedroom-2",
+      "name": "Bedroom 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.4577106293664581,
+        "pitch": -0.07631791894084294,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9397877176490912,
+          "pitch": -0.027699615056224758,
+          "rotation": 5.497787143782138,
+          "target": "0-living--tv-unit"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "RAJEEV 3D",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
